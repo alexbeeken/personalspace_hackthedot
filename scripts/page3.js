69e8e56b3a978a7270edc6b136da67f7f9ab2de4
Andrew Haupt <ajhaupt7@gmail.com>
2016-02-04 20:40:08 -0800

    dafds

--- scripts/page3.js
+++ scripts/page3.js
@@ -2,5 +2,8 @@ $(function() {
   $('.yes-button').on("click", function() {
     $('.yes-button').css({background:'#b00100'});
     $('.good').delay(500).fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(1000);
-  });  
+  }); 
+  $('#alone').on("click", function() {
+    $('#alone').fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200).fadeOut(200).fadeIn(200);
+  }
 });
